0c550a944a5ecf90a9c36469c97845e0b197500e
Sean Boling <sean.boling@gmail.com>
2023-03-13 00:26:46 -0600

    add some debugging code to play around with  and  super-globals and discover what is available

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    // $authorId = filter_input(INPUT_GET, 'authorId', FILTER_SANITIZE_NUMBER_INT);
+    // $categoryId = filter_input(INPUT_GET, 'categoryId', FILTER_SANITIZE_NUMBER_INT);
+
+    // echo isset($_GET['authorId']) . '<br>';
+
+    // echo $authorId . '<br>';
+    // echo $categoryId;
+
+    // echo '<pre>';
+    // print_r($_GET);
+    // echo '<pre>';
+
+    // echo '<pre>';
+    // print_r($_SERVER);
+    // echo '<pre>';
+?>
