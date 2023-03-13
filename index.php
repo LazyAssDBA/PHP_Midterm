de7d5e44ceac433dece2bd794ca17cb555b4a1a3
Sean Boling <sean.boling@gmail.com>
2023-03-13 13:58:46 -0600

    added html template and <h1> containing my name to index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MidTerm Project</title>
+</head>
+<body>
+    <h1>Sean Boling - Midterm - INF653</h1>
+    <?php
     // $authorId = filter_input(INPUT_GET, 'authorId', FILTER_SANITIZE_NUMBER_INT);
     // $categoryId = filter_input(INPUT_GET, 'categoryId', FILTER_SANITIZE_NUMBER_INT);
 
@@ -14,4 +24,6 @@
     // echo '<pre>';
     // print_r($_SERVER);
     // echo '<pre>';
-?>
+?>    
+</body>
+</html>
