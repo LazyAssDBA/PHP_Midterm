883e865966ecae07a34f39ae424810509b427793
Sean Boling <sean.boling@gmail.com>
2023-03-20 16:06:35 -0600

    Trying to fix FAILURE: /authors/endpoint should return an array of JSON data

--- api/authors/read.php
+++ api/authors/read.php
@@ -8,7 +8,7 @@
     if($num > 0) {
         // Author array
         $author_arr = array();
-        $author_arr['data'] = array();
+        //$author_arr['data'] = array();
 
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row);
@@ -18,8 +18,8 @@
                 'author' => $author
             );
 
-            // Push to "data"
-            array_push($author_arr['data'], $author_item);
+            // Push to array
+            array_push($author_arr, $author_item);
         }
 
         // Turn to JSON & output
