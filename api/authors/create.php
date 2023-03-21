ddca6f427469e2120fac70cd60a3f1834c107105
Sean Boling <sean.boling@gmail.com>
2023-03-20 23:29:26 -0600

    added logic for Missing Required Parameters and returning deleted id

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,14 +1,23 @@
 <?php
-    $author->author = $data->author;
-
-    // Create author
-    if($author->create()) {
-        echo json_encode(
-            array('message' => 'Author Created')
-        );
+    // Make sure all paremeters are passed in
+    if(!property_exists($data, 'author')) {
+        echo json_encode(array('message' => 'Missing Required Parameters'));
     } else {
-        echo json_encode(
-            array('message' => 'Author Not Created')
-        );
+        // Set the author to create
+        $author->author = $data->author;
+
+        // Create author
+        if($author->create()) {
+            // Create JSON array for output to user
+            $author_arr = array (
+                'id' => $db->lastInsertId(),
+                'author' => $author->author
+            );
+            echo json_encode($author_arr);
+        } else {
+            echo json_encode(
+                array('message' => 'Author Not Created')
+            );
+        }
     }
 ?>
