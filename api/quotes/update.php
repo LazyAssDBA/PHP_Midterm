f243170db992488dc3e595d545a1fe85084654d9
Sean Boling <sean.boling@gmail.com>
2023-03-21 14:51:58 -0600

    added 'Missing Required Paramets' section

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -1,38 +1,45 @@
 <?php
-    // Include author and category Models
-    include_once '../../models/Author.php';
-    include_once '../../models/Category.php';
-
-    // Instantiate author and category objects
-    $author = new Author($db);
-    $category = new Category($db);
-
-    // Check if author and category are valid
-    if(!isValid($data->author_id, $author)) {
-        echo json_encode(array('message' => 'author_id Not Found'));
-    } elseif(!isValid($data->category_id, $category)){        
-        echo json_encode(array('message' => 'category_id Not Found'));
+    // Make sure all paremeters are passed in
+        if(!property_exists($data, 'quote') 
+        || !property_exists($data, 'author_id')
+        || !property_exists($data, 'category_id')) {
+            echo json_encode(array('message' => 'Missing Required Parameters'));
     } else {
-        // Set quote to update
-        //$quote->id = $data->id;
-        $quote->quote = $data->quote;
-        $quote->author_id = $data->author_id;
-        $quote->category_id = $data->category_id;
+        // Include author and category Models
+        include_once '../../models/Author.php';
+        include_once '../../models/Category.php';
 
-        // Update quote
-        if($quote->update()) {
-            // Create JSON array for output to user
-            $quote_arr = array (
-                'id' => $quote->id,
-                'quote' => $quote->quote, 
-                'author_id' => $quote->author_id,
-                'category_id' => $quote->category_id
-            );
-            echo json_encode($quote_arr);
+        // Instantiate author and category objects
+        $author = new Author($db);
+        $category = new Category($db);
+
+        // Check if author and category are valid
+        if(!isValid($data->author_id, $author)) {
+            echo json_encode(array('message' => 'author_id Not Found'));
+        } elseif(!isValid($data->category_id, $category)){        
+            echo json_encode(array('message' => 'category_id Not Found'));
         } else {
-            echo json_encode(
-                array('message' => 'No Quotes Found')
-            );
+            // Set quote to update
+            //$quote->id = $data->id;
+            $quote->quote = $data->quote;
+            $quote->author_id = $data->author_id;
+            $quote->category_id = $data->category_id;
+
+            // Update quote
+            if($quote->update()) {
+                // Create JSON array for output to user
+                $quote_arr = array (
+                    'id' => $quote->id,
+                    'quote' => $quote->quote, 
+                    'author_id' => $quote->author_id,
+                    'category_id' => $quote->category_id
+                );
+                echo json_encode($quote_arr);
+            } else {
+                echo json_encode(
+                    array('message' => 'No Quotes Found')
+                );
+            }
         }
     }
 ?>
