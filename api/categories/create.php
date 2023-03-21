b322c1ced545763e0c5937eddabbe95d78380320
Sean Boling <sean.boling@gmail.com>
2023-03-20 23:35:28 -0600

    added logic for Missing Required Parameters and returning created id

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,14 +1,23 @@
 <?php
-    $category->category = $data->category;
-
-    // Create category
-    if($category->create()) {
-        echo json_encode(
-            array('message' => 'Category Created')
-        );
+    // Make sure all paremeters are passed in
+    if(!property_exists($data, 'category')) {
+        echo json_encode(array('message' => 'Missing Required Parameters'));
     } else {
-        echo json_encode(
-            array('message' => 'Category Not Created')
-        );
+        // Set the category to create
+        $category->category = $data->category;
+
+        // Create category
+        if($category->create()) {
+            // Create JSON array for output to user
+            $category_arr = array (
+                'id' => $db->lastInsertId(),
+                'category' => $category->category
+            );
+            echo json_encode($category_arr);
+        } else {
+            echo json_encode(
+                array('message' => 'Category Not Created')
+            );
+        }
     }
 ?>
