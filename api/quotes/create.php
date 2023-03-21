a5816cb64c50df2e149d0062652206780ef62f55
Sean Boling <sean.boling@gmail.com>
2023-03-21 12:02:53 -0600

    Added 'Missing Required Parameters', 'author_id Not Found', and 'category_id Not Found' logic

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -1,16 +1,45 @@
 <?php
-    $quote->quote = $data->quote;
-    $quote->author_id = $data->author_id;
-    $quote->category_id = $data->category_id;
 
-    // Create quote
-    if($quote->create()) {
-        echo json_encode(
-            array('message' => 'Quote Created')
+    // Make sure all paremeters are passed in
+    if(!property_exists($data, 'quote') 
+    || !property_exists($data, 'author_id')
+    || !property_exists($data, 'category_id')) {
+        echo json_encode(array('message' => 'Missing Required Parameters'));
+} else {
+    // Include author and category Models
+    include_once '../../Models/Author.php';
+    include_once '../../Models/Category.php';
+
+    // Instantiate author and category objects
+    $author = new Author($db);
+    $category = new Category($db);
+
+    // Check if author and category are valid
+    if(!isValid($data->author_id, $author)) {
+        echo json_encode(array('message' => 'author_id Not Found'));
+    } elseif(!isValid($data->category_id, $category)){        
+        echo json_encode(array('message' => 'category_id Not Found'));
+    } else { 
+        // Set the quote to create
+        $quote->quote = $data->quote;
+        $quote->author_id = $data->author_id;
+        $quote->category_id = $data->category_id;
+
+        // Create quote
+        if($quote->create()){
+        } else {
+            echo json_encode(array('message' => 'No Quotes Found'));
+        }
+
+        // Create JSON array for output to user
+        $quote_arr = array (
+            'id' => $db->lastInsertId(),
+            'quote' => $quote->quote, 
+            'author_id' => $quote->author_id,
+            'category_id' => $quote->category_id
         );
-    } else {
-        echo json_encode(
-            array('message' => 'Quote Not Created')
-        );
+
+        echo json_encode($quote_arr);
     }
+}
 ?>
