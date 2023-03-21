bc8f93c92ada0751a82a0dc474209992f10e2931
Sean Boling <sean.boling@gmail.com>
2023-03-21 00:14:26 -0600

    Fixing quote JSON not displaying correctly

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -1,12 +1,11 @@
 <?php
     // Get id from URL
-    $quote->id = isset($_GET['id']) ? $_GET['id'] : die();
+    //$quote->id = isset($_GET['id']) ? $_GET['id'] : die();
 
     // Get quote
     $quote->read_single();
 
     // Create array
-    if ($quote->quote) {
     $quote_arr = array(
         'id' => $quote->id,
         'quote' => $quote->quote,
@@ -14,12 +13,7 @@
         'author' => $quote->author,
         //'category_id' => $quote->category_id,
         'category' => $quote->category
-    );} else {
-        // No Quote
-        echo json_encode(
-            array('message:' => 'quote_id Not Found')
-        );
-    }
+    );
 
     // Make JSON
     echo json_encode($quote_arr);
